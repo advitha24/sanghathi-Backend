ab4e16ab0ebb74b0b148fbd4380d18e6ac495ceb
Mondalgithub <shovanmondal2004@gmail.com>
2025-04-07 20:09:52 +0530

    Local Guardian Controller edit

--- src/controllers/Student/localGuardianController.js
+++ src/controllers/Student/localGuardianController.js
@@ -1,27 +1,10 @@
 import LocalGuardian from '../../models/Student/LocalGuardian.js';
 import catchAsync from '../../utils/catchAsync.js';
 import AppError from '../../utils/appError.js';
-import LocalGuardian from '../../models/Student/LocalGuardian.js';
-import catchAsync from '../../utils/catchAsync.js';
-import AppError from '../../utils/appError.js';
 
 export const createOrUpdateLocalGuardian = catchAsync(async (req, res, next) => {
   const { userId, ...guardianData } = req.body;
-  const { userId, ...guardianData } = req.body;
 
-  if (!userId) {
-    return next(new AppError('User ID is required', 400));
-  }
-
-  const localGuardian = await LocalGuardian.findOneAndUpdate(
-    { userId },
-    guardianData,
-    { 
-      new: true,
-      upsert: true,
-      runValidators: true
-    }
-  );
   if (!userId) {
     return next(new AppError('User ID is required', 400));
   }
@@ -37,8 +20,6 @@ export const createOrUpdateLocalGuardian = catchAsync(async (req, res, next) => 
   );
 
   res.status(200).json({
-    status: 'success',
-    data: { localGuardian }
     status: 'success',
     data: { localGuardian }
   });
@@ -47,7 +28,6 @@ export const createOrUpdateLocalGuardian = catchAsync(async (req, res, next) => 
 export const getLocalGuardianByUserId = catchAsync(async (req, res, next) => {
   const { userId } = req.params;
 
-
   const localGuardian = await LocalGuardian.findOne({ userId });
 
   if (!localGuardian) {
@@ -55,15 +35,9 @@ export const getLocalGuardianByUserId = catchAsync(async (req, res, next) => {
       status: 'success',
       data: null
     });
-    return res.status(200).json({
-      status: 'success',
-      data: null
-    });
   }
 
   res.status(200).json({
-    status: 'success',
-    data: { localGuardian }
     status: 'success',
     data: { localGuardian }
   });
