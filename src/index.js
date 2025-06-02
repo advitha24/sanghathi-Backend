f9ff1c9642ce9cf7a7bb665b59ecf8cdb827b8ec
shovan-mondal <shovanmondal2004@gmail.com>
2025-06-02 21:29:56 +0530

    Fixed validation Error for X Forwarded For in production

--- src/index.js
+++ src/index.js
@@ -58,6 +58,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Trust proxy - Add this before other middleware
+app.set('trust proxy', 1);
+
 //1) GLOBAL MIDDLEWARE
 // Configure CORS to allow requests from any origin during development
 app.use(
