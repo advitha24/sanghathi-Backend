ef69cd9634485ee1df75ce39cc062002a9d45382
Advitha K <advitha24@gmail.com>
2025-09-26 19:48:59 +0530

    Update parentDetailsController.js

--- src/controllers/Student/parentDetailsController.js
+++ src/controllers/Student/parentDetailsController.js
@@ -1,7 +1,7 @@
 import ParentDetails from "../../models/Student/ParentDetails.js";
 import catchAsync from "../../utils/catchAsync.js";
 import AppError from "../../utils/appError.js";
-import User from "../../models/User.js"; // Assuming you need the User model
+import User from "../../models/User.js";
 
 // Create or Update Parent Details Data
 export const createOrUpdateParentDetails = catchAsync(async (req, res, next) => {
@@ -14,18 +14,22 @@ export const createOrUpdateParentDetails = catchAsync(async (req, res, next) => 
     motherMiddleName,
     motherLastName,
     fatherOccupation,
+    fatherOrganization,
+    fatherDesignation,
+    fatherOfficePhone,
+    fatherOfficeAddress,
+    fatherAnnualIncome,
     motherOccupation,
-    fatherIncome,
-    motherIncome,
-    fatherMobileNumber,
-    motherMobileNumber,
-    address,
-    state,
-    pincode,
+    motherOrganization,
+    motherDesignation,
+    motherOfficePhone,
+    motherOfficeAddress,
+    motherAnnualIncome,
   } = req.body;
 
   try {
-    console.log("Create or update reached")
+    console.log("Create or update reached");
+
     const updatedParentDetails = await ParentDetails.findOneAndUpdate(
       { userId },
       {
@@ -36,14 +40,17 @@ export const createOrUpdateParentDetails = catchAsync(async (req, res, next) => 
         motherMiddleName,
         motherLastName,
         fatherOccupation,
+        fatherOrganization,
+        fatherDesignation,
+        fatherOfficePhone,
+        fatherOfficeAddress,
+        fatherAnnualIncome,
         motherOccupation,
-        fatherIncome,
-        motherIncome,
-        fatherMobileNumber,
-        motherMobileNumber,
-        address,
-        state,
-        pincode,
+        motherOrganization,
+        motherDesignation,
+        motherOfficePhone,
+        motherOfficeAddress,
+        motherAnnualIncome,
       },
       { new: true, upsert: true }
     );
@@ -61,76 +68,81 @@ export const createOrUpdateParentDetails = catchAsync(async (req, res, next) => 
 
 // Get Parent Details Data by User ID
 export const getParentDetailsByUserId = catchAsync(async (req, res, next) => {
-    console.log("Route handler triggered, req.params:", req.params);
-    const { userId } = req.params; // Changed from id to userId
-    console.log("Querying for userId:", userId);
-    const parentDetails = await ParentDetails.findOne({ userId: userId });
-  
-    if (!parentDetails) {
-      return next(new AppError("Parent details not found", 404));
-    }
-  
-    res.status(200).json({
-      status: "success",
-      data: {
-        parentDetails,
-      },
-    });
-  });
+  console.log("Route handler triggered, req.params:", req.params);
+  const { userId } = req.params;
 
-// Get all Parent Details data with user details (similar to getAllCareer)
+  console.log("Querying for userId:", userId);
+
+  const parentDetails = await ParentDetails.findOne({ userId });
+
+  if (!parentDetails) {
+    return next(new AppError("Parent details not found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      parentDetails,
+    },
+  });
+});
+
+// Get all Parent Details data with user details
 export const getAllParentDetails = catchAsync(async (req, res, next) => {
-    const parentDetailsData = await User.aggregate([
-      {
-        $match: {
-          role: "student",
+  const parentDetailsData = await User.aggregate([
+    {
+      $match: {
+        role: "student",
+      },
+    },
+    {
+      $lookup: {
+        from: "parentdetails", // ⚠️ Mongo collection name is lowercase
+        localField: "_id",
+        foreignField: "userId",
+        as: "parentDetails",
+      },
+    },
+    {
+      $unwind: {
+        path: "$parentDetails",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        role: 1,
+        parentDetails: {
+          fatherFirstName: "$parentDetails.fatherFirstName",
+          fatherMiddleName: "$parentDetails.fatherMiddleName",
+          fatherLastName: "$parentDetails.fatherLastName",
+          motherFirstName: "$parentDetails.motherFirstName",
+          motherMiddleName: "$parentDetails.motherMiddleName",
+          motherLastName: "$parentDetails.motherLastName",
+          fatherOccupation: "$parentDetails.fatherOccupation",
+          fatherOrganization: "$parentDetails.fatherOrganization",
+          fatherDesignation: "$parentDetails.fatherDesignation",
+          fatherOfficePhone: "$parentDetails.fatherOfficePhone",
+          fatherOfficeAddress: "$parentDetails.fatherOfficeAddress",
+          fatherAnnualIncome: "$parentDetails.fatherAnnualIncome",
+          motherOccupation: "$parentDetails.motherOccupation",
+          motherOrganization: "$parentDetails.motherOrganization",
+          motherDesignation: "$parentDetails.motherDesignation",
+          motherOfficePhone: "$parentDetails.motherOfficePhone",
+          motherOfficeAddress: "$parentDetails.motherOfficeAddress",
+          motherAnnualIncome: "$parentDetails.motherAnnualIncome",
         },
       },
-      {
-        $lookup: {
-          from: "parentDetails",
-          localField: "_id",
-          foreignField: "userId",
-          as: "parentDetails",
-        },
-      },
-      {
-        $unwind: {
-          path: "$parentDetails",
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      {
-        $project: {
-          _id: 1,
-          name: 1,
-          role: 1,
-          parentDetails: {
-            fatherFirstName: "$parentDetails.fatherFirstName",
-            fatherMiddleName: "$parentDetails.fatherMiddleName",
-            fatherLastName: "$parentDetails.fatherLastName",
-            motherFirstName: "$parentDetails.motherFirstName",
-            motherMiddleName: "$parentDetails.motherMiddleName",
-            motherLastName: "$parentDetails.motherLastName",
-            fatherOccupation: "$parentDetails.fatherOccupation",
-            motherOccupation: "$parentDetails.motherOccupation",
-            fatherIncome: "$parentDetails.fatherIncome",
-            motherIncome: "$parentDetails.motherIncome",
-            fatherMobileNumber: "$parentDetails.fatherMobileNumber",
-            motherMobileNumber: "$parentDetails.motherMobileNumber",
-            address: "$parentDetails.address",
-            state: "$parentDetails.state",
-            pincode: "$parentDetails.pincode",
-          },
-        },
-      },
-    ]);
+    },
+  ]);
 
-    res.status(200).json({
-      status: "success",
-      data: parentDetailsData,
-    });
+  res.status(200).json({
+    status: "success",
+    data: parentDetailsData,
   });
+});
 
 // Delete Parent Details Data by User ID
 export const deleteParentDetailsByUserId = catchAsync(async (req, res, next) => {
