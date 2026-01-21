694758a108944d22fc3c34bd58a62988f658f127
Advitha Kannan <advitha@Advithas-MacBook-Air.local>
2026-01-21 20:55:20 +0530

    updated mentor mentee conversation

--- src/models/Conversation.js
+++ src/models/Conversation.js
@@ -1,65 +1,70 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-  conversationId: {
-    type: String,
-    required: false,
-  },
-  status: {
-    type: String,
-    default: "closed",
-    enum: ["active", "closed"],
-  },
-  mentorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: false,
-  },
-  menteeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: false,
-  },
-  title: {
-    type: String,
-    default: "",
-  },
-  topic: {
-    type: String,
-    default: "",
-  },
-  conversationText: {
-    type: String,
-    default: "",
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  moocChecked: {
-    type: Boolean,
-    default: false,
-  },
-  projectChecked: {
-    type: Boolean,
-    default: false,
-  },
-  summary: {
-    type: String,
-    default: "",
-  },
-  isOffline: {
-    type: Boolean,
-    default: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+ conversationId: {
+   type: String,
+   required: false,
+ },
+ status: {
+   type: String,
+   default: "closed",
+   enum: ["active", "closed"],
+ },
+ mentorId: {
+   type: mongoose.Schema.Types.ObjectId,
+   ref: "User",
+   required: false,
+ },
+ menteeId: {
+   type: mongoose.Schema.Types.ObjectId,
+   ref: "User",
+   required: false,
+ },
+ title: {
+   type: String,
+   default: "",
+ },
+ topic: {
+   type: String,
+   default: "",
+ },
+ conversationText: {
+   type: String,
+   default: "",
+ },
+ description: {
+   type: String,
+   default: "",
+ },
+ moocChecked: {
+   type: Boolean,
+   default: false,
+ },
+ projectChecked: {
+   type: Boolean,
+   default: false,
+ },
+ summary: {
+   type: String,
+   default: "",
+ },
+ semester: {
+   type: Number,
+   required: false,
+ },
+ academicYear: {
+   type: String,
+   default: "2025-26",
+ },
+ isOffline: {
+   type: Boolean,
+   default: true,
+ },
+ date: {
+   type: Date,
+   default: Date.now,
+ },
 });
 
 const Conversation = mongoose.model("Conversation", conversationSchema);
 export default Conversation;
-
-
-
